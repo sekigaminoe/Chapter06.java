1680e74218b806e7c0835fd9d48f811c6a6f027a
NoeSekigami <141192154+sekigaminoe@users.noreply.github.com>
2023-12-04 17:42:56 +0900

    確認問題完了

--- src/Chapter06.java
+++ src/Chapter06.java
@@ -3,7 +3,26 @@ public class Chapter06 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		long l = 10000000000L;
+		System.out.println(l);
+		
+		double d = 3.14d;
+		System.out.println(d);
+		
+		boolean b = true;
+		System.out.println(b);
+		
+		Integer i = 100;
+		System.out.println(i);
+		
+		Float f = 1.4142f;
+		System.out.println(f);
+		
+		Character c = 'う';
+		System.out.println(c);
+		
+		int [] array = { 0, 10, 20, 30, 40 };
+		System.out.println(array[3]);
 	}
 
 }
